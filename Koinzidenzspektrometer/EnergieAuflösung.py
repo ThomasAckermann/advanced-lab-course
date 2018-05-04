d36345201af7bfa4e5af6bb9e9f788d3addc0a27
ThomasAckermann <30625654+ThomasAckermann@users.noreply.github.com>
2018-05-04 18:17:34 +0200

    energieauflösung

--- Koinzidenzspektrometer/EnergieAuflösung.py
+++ Koinzidenzspektrometer/EnergieAuflösung.py
@@ -1,5 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+
+def func(x, m, c):
+    return c * np.exp(-1 * m * x)
 
 
 sigma = np.array([17.6, 19.8, 13.2, 13.7, 10.8, 19.8, 4.1, 14.5, 16.7])
@@ -12,7 +17,13 @@ energy = np.array([1.17323, 1.32248, 0.6616, 0.8358, 0.511, 1.2746, 0.081, 0.302
 relative_energy = fwhm_energy / energy
 relative_energy_err = fwhm_energy_err / energy
 
-plt.errorbar(energy, relative_energy, yerr=relative_energy_err, marker='.', lw=0)
+popt, pcov = curve_fit(func, energy[0:-2], relative_energy[0:-2], sigma=relative_energy_err[0:-2])
+
+x = np.linspace(0.08, 1.35, 1000)
+
+plt.errorbar(energy, relative_energy, yerr=relative_energy_err, marker='.', lw=0, label='Messdaten')
+plt.plot(x, func(x, *popt), label='Fit')
+plt.legend(loc='upper right')
 plt.xscale('log')
 plt.ylabel('Relative Energie $\\frac{\Delta E}{E}$')
 plt.xlabel('Energie $E$')
