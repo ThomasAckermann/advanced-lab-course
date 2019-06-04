60008d3f0aa3951560dab6f4f70267dafd98141e
ThomasAckermann <30625654+ThomasAckermann@users.noreply.github.com>
2019-06-04 15:19:17 +0200

    Quellenverzeichnis

---
+++ Atmosphärische_Spurengase/fp18/Thomas_Aaron/hal_log.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+# rcParams['font.family'] = 'sans-serif'
+# rcParams['font.sans-serif'] = ['Linux Biolinum']
+# rcParams['font.size'] = 20
+rcParams['text.usetex'] = True
+# rcParams['text.latex.preamble'] = '\usepackage{libertine}'
+
+data = np.loadtxt('log_1.txt')
+x = data[:,0]
+y = data[:,1]
+
+plt.plot(x, y, 'ks', markersize=2)
+plt.xlabel('Channels')
+plt.ylabel('Optical Density')
+plt.savefig('hal_log.pdf')
