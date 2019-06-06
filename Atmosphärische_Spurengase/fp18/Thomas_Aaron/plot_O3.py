bc2f47ebefe49f71c7da7713a8291e6dfa3d7ffb
ThomasAckermann <30625654+ThomasAckermann@users.noreply.github.com>
2019-06-06 12:31:54 +0200

    VCD Bild O3 erstellt

--- Atmosphärische_Spurengase/fp18/Thomas_Aaron/plot_O3.py
+++ Atmosphärische_Spurengase/fp18/Thomas_Aaron/plot_O3.py
@@ -29,27 +29,33 @@ def linear(x, a, c):
     return a * x + c
 
 # popt, pcov = curve_fit(linear, AMF[:416],np.abs(result_of_convolution_O3[:416]),p0=[1.0 * 10**19,-1.0 * 10**19])
-popt, pcov = curve_fit(linear, AMF[:416], result_of_convolution_O3[:416],p0=[1.0 * 10**19,-1.0 * 10**19])
+popt_1, pcov_1 = curve_fit(linear, AMF[:416], result_of_convolution_O3[:416],p0=[1.0 * 10**19,-1.0 * 10**19])
+popt_2, pcov_2 = curve_fit(linear, AMF[380:394], result_of_convolution_O3[380:394],p0=[1.0 * 10**19,-1.0 * 10**19])
+
 
 #plot
-k = np.linspace(1,3.5,500)
+k = np.linspace(-0.1,3.5,500)
 # plot = plt.errorbar(AMF[:416],np.abs(result_of_convolution_O3[:416]), yerr=result_of_convolution_O3_error[:416], label='Messdaten für O3',fmt = '.')
 plot = plt.errorbar(AMF[:416], result_of_convolution_O3[:416], yerr=result_of_convolution_O3_error[:416], label='Messdaten für O3',fmt = '.')
+plt.plot(k ,linear(k , *popt_1), 'r-', label='Fit 1', color='black') 
+plt.plot(k ,linear(k , *popt_2), 'r-', label='Fit 2', color='black') 
 
-plt.plot(k ,linear(k , *popt), 'r-', label='Fit', color='black') 
+plt.legend()
 axes = plt.gca()
-plt.ylabel("$\Delta$SCD für O3")
+plt.ylabel("$\Delta$SCD für O$_3$")
 plt.xlabel("AMF")
-
-#nötig falls gegen Uhrzeit geplottet wird
-#for index, label in enumerate(axes.xaxis.get_ticklabels()):
-#    if index % 45 !=0:
-#        label.set_visible(False)
 
 f_gcf = plt.gcf()
 f_gcf.set_size_inches(11.69, 8.27)
 plt.show()
-print(popt, pcov)
-print('Steigung:', popt[0])
-print('y_Achsenabschnitt:', popt[1])
-print('Kovarianz:', np.sqrt(pcov[0][0]))
+print(popt_1, pcov_1)
+print('Steigung:', popt_1[0])
+print('y_Achsenabschnitt:', popt_1[1])
+print('Kovarianz:', np.sqrt(pcov_1[0][0]))
+
+
+print(popt_2, pcov_2)
+print('Steigung:', popt_2[0])
+print('y_Achsenabschnitt:', popt_2[1])
+print('Kovarianz:', np.sqrt(pcov_2[0][0]))
+
--- Atmosphärische_Spurengase/fp18/Thomas_Aaron/plot_VCD_O3.py
+++ Atmosphärische_Spurengase/fp18/Thomas_Aaron/plot_VCD_O3.py
@@ -3,6 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from datetime import datetime
+from matplotlib import rcParams
+import datetime
+rcParams['font.size'] = 17
+rcParams['text.usetex'] = True
+# rcParams['text.latex.preamble'] = '\usepackage{libertine}'
+
 
 #import data
 
@@ -25,24 +31,28 @@ result_of_convolution_NO2_error  = np.genfromtxt(dateiname, skip_header=1,usecol
 result_of_convolution_O4  = np.genfromtxt(dateiname, skip_header=1,usecols=(13))
 result_of_convolution_O4_error  = np.genfromtxt(dateiname, skip_header=1,usecols=(14))
 
-VCD = (6*10**18+result_of_convolution_O3) * np.cos(2 * np.pi * SZA / 360)
-VCD_error =(6*10**18 + result_of_convolution_O3_error) * np.cos(2 * np.pi * SZA / 360)
+VCD = (7.9325*10**18+result_of_convolution_O3) * np.cos(2 * np.pi * SZA / 360)
+VCD_error =np.sqrt((result_of_convolution_O3_error*np.cos(2*np.pi*SZA/360))**2 + (0.017*10**18*np.cos(2*np.pi*SZA/360))**2)
 
 # plot
 k = np.linspace(0,3.5,500)
-plot = plt.errorbar(StartZeit, VCD, yerr=VCD_error, label='Messdaten für O3',fmt='.')
+plot = plt.errorbar(StartZeit, VCD, yerr=VCD_error, label='Messdaten für O3',fmt='ks', markersize=4)
 # plot = plt.errorbar(StartZeit, VCD, label='Messdaten für O3', fmt='.', markersize=3)
 axes = plt.gca()
 plt.ylabel("VCD für O3")
 plt.xlabel("Uhrzeit UCT")
 
 # nötig falls gegen Uhrzeit geplottet wird
+a = []
+for i in range(len(axes.get_xticks())):
+    if (i%45 == 0):
+        a.append(i)
 
-for index, label in enumerate(axes.xaxis.get_ticklabels()):
-    if index % 45 !=0:
-        label.set_visible(False)
-
+ticks = []
+for i in a:
+    ticks.append(axes.get_xticks()[i])
+axes.set_xticks(ticks)
 f_gcf = plt.gcf()
 f_gcf.set_size_inches(11.69, 8.27)
-plt.show()
+# plt.show()
 plt.savefig('VCD_O3_over_day.pdf')
